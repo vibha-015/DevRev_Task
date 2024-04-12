7441c6667a727535f1445e513aaf55f068a9a8bf
Vibha <114283633+vibha-015@users.noreply.github.com>
2024-04-12 12:33:26 +0530

    Delete Task-2/FINAL_CODE directory

--- Task-2/FINAL_CODE/code/dist/function-factory.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const functionFactory: {
-    readonly function_1: (events: any[]) => Promise<void>;
-    readonly function_2: (events: any[]) => Promise<void>;
-};
-export type FunctionFactoryType = keyof typeof functionFactory;
--- Task-2/FINAL_CODE/code/jest.config.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2023 DevRev, Inc. All rights reserved.
- */
-
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
